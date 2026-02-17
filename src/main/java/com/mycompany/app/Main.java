446bc7f6380cda179f4e8281d94368e4b471e15b
Ruben Gonzalez Castro <ruben.culiacan@gmail.com>
2026-02-17 22:42:48 +0000

    exercises 4.4

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,5 +1,9 @@
 package com.mycompany.app;
 
+import java.util.Random;
+
+import com.mycompany.app.entities.Book;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -9,5 +13,23 @@ public class Main {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_persistence_unit");
 
     EntityManager em = emf.createEntityManager(); // Represent the persistence context
-}
+    Random rnd = new Random();
+    try {
+      em.getTransaction().begin();
+      for(int i=0;i<11;i++){
+        Book book = new Book();
+        int rndNum = rnd.nextInt(100);
+        book.setName("My Book "+i+" Num " + rndNum);
+        book.setIsbn("123-" + i + "-" + rndNum);
+        em.persist(book);
+      }
+      em.getTransaction().commit();
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    finally{
+      em.close();
+    }
+  }
 }
